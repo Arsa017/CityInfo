6a910e01e54bbad04613637fa802cbce47f22475
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-03 16:14:18 +0100

    Refactoring CitiesController and adding GetCity method
    GetCity returs city for sended id city paramter

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -9,12 +9,16 @@ namespace CityInfo.Api.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-               return new JsonResult(
-                new List<object>
-                {
-                    new {id = 1, Name = "New York City"},
-                    new {id = 2, Name = "Belgrade"}
-                });
+            return new JsonResult(
+            CityDataStore.Current.Cities
+            );
         }
+
+        [HttpGet("{id}")]       // to work with paramaters currly brackets are used!
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        } 
     }
 }
