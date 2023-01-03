da4de9f7836aa5622f0f4ab73b24ceffd3e051e6
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-03 16:13:01 +0100

    Adding CityDto Model and CityDataStore

---
+++ CityInfo.Api/CityDataStore.cs
@@ -0,0 +1,33 @@
+﻿using CityInfo.Api.Model;
+
+namespace CityInfo.Api
+{
+    public class CityDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CityDataStore Current { get; } = new CityDataStore();  // singleton pattern design
+        public CityDataStore()
+        {
+            Cities= new List<CityDto>()
+                {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDto() {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finnished."
+                },
+                new CityDto() {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower."
+                }
+            };
+        }
+    }
+}
