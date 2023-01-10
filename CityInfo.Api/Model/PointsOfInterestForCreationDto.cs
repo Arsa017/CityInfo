ff6bb25b3c6d12995b700614eee744426eaaeaed
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-10 22:37:08 +0100

    Adding validation attributes for model PointsOfInterestForCreationDto

--- CityInfo.Api/Model/PointsOfInterestForCreationDto.cs
+++ CityInfo.Api/Model/PointsOfInterestForCreationDto.cs
@@ -1,9 +1,13 @@
-﻿namespace CityInfo.Api.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.Api.Model
 {
     public class PointsOfInterestForCreationDto
     {
-
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
