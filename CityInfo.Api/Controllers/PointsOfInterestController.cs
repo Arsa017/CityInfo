18b35795364a04958122457fc07ea03614e8600e
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-10 23:49:14 +0100

    Fully and partial update
    Addint methods UpdatePointOfInterest and PartiallyUpdatePointOfInterest in  PointsOfInterestController

--- CityInfo.Api/Controllers/PointsOfInterestController.cs
+++ CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using CityInfo.Api.Model;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
@@ -44,7 +45,7 @@ namespace CityInfo.Api.Controllers
             return Ok(pointOfInterest);
         }
 
-        [HttpPost]
+        [HttpPost]  // adding new resource with provided values
         public ActionResult<PointOfInterestDto> CreatePointOfInterest(      // returns created resourse in the response
             int cityId, [FromBody] PointsOfInterestForCreationDto pointOfInterest)
         {
@@ -75,6 +76,73 @@ namespace CityInfo.Api.Controllers
                 },
                 value: finalPointOfInterest
                 );
+        }
+
+        [HttpPut("{pointOfInterestId}")]   // fully update of resource; consumer of the API must provide values for ALL fields for resource
+        public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId,
+           [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            var city = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // find point of interest
+            var pointOfInterestFromStore = city.PointOfInterests.FirstOrDefault(
+                p => p.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{pointOfInterestId}")]    // pratial update of resource; update only for fields specified into the patchDocument    
+        public ActionResult PartiallyUpdatePointOfInterest(
+            int cityId, int pointOfInterestId,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument)
+        {
+            var city = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city is null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointOfInterests
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterestFromStore is null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch =
+                new PointOfInterestForUpdateDto()
+                {
+                    Name = pointOfInterestFromStore.Name,
+                    Description = pointOfInterestFromStore.Description
+                };
+
+            patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);          // applying patch document on the object that we want to patch
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!TryValidateModel(pointOfInterestToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+        
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
         }
 
     }
