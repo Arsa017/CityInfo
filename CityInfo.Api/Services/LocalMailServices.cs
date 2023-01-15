bfc00ab93f351710ca7814fb47f3034b94865b73
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-16 00:28:43 +0100

    Working with configuration files

--- CityInfo.Api/Services/LocalMailServices.cs
+++ CityInfo.Api/Services/LocalMailServices.cs
@@ -2,8 +2,14 @@
 {
     public class LocalMailServices : IMailServices
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreplay@mycompany.com";
+        private readonly string _mailTo = String.Empty;
+        private readonly string _mailFrom = String.Empty;
+
+        public LocalMailServices(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
 
         public void Send(string subject, string message)
         {
