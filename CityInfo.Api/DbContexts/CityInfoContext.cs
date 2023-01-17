d3050945b7d4eba4454c314ed68e95fc8a455395
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-18 00:30:35 +0100

    Adding Entity framework core
    Entities and DbContext

---
+++ CityInfo.Api/DbContexts/CityInfoContext.cs
@@ -0,0 +1,24 @@
+﻿using CityInfo.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.Api.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) 
+            :base(options)
+        {
+        
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+    }
+}
