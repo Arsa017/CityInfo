e48a8af62f92df5db07ec783853999304d84934c
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-01-25 00:23:16 +0100

    Returning data from CityInfoRepository when requesting resources

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.Api.Model;
+using CityInfo.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.Api.Controllers
@@ -7,31 +8,48 @@ namespace CityInfo.Api.Controllers
     [Route(("api/cities"))]
     public class CitiesController : ControllerBase
     {
-        private readonly CityDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(CityDataStore citiesDataStore)
+        public CitiesController(ICityInfoRepository cityInfoRepository)             // we want to inject contract, not the exact implementation
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+
+            var result = new List<CityWithoutPointsOfInterestDto>();                // mapping enities to CityWithoutPointsOfInterestDto model
+            foreach (var cityEntity in cityEntities)                    
+            {
+                result.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description
+                });
+            }
+
+            return Ok(result);
+            //  return Ok(_citiesDataStore.Cities);
         }
 
         [HttpGet("{id}")]       // to work with paramaters currly brackets are used!
         public ActionResult<CityDto> GetCity(int id)
         {
-            // find city
-            var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
 
-            if (city == null)
-            {
-                return NotFound();
-            }
 
-            return Ok(city);
+            //// find city
+            //var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+
+            //if (city == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return Ok(city);
+            return Ok();
         } 
     }
 }
